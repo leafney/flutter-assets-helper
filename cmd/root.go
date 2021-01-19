5df9e4bc63e14fd63bbccf2d4988d80aac93be48
leafney <babycoolzx@126.com>
2021-01-19 20:37:47 +0800

    test fsnotify

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Used for flags.
+	cfgFile     string
+	userLicense string
+
+	rootCmd = &cobra.Command{
+		Use:   "cobra",
+		Short: "A generator for Cobra based Applications",
+		Long: `Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	}
+)
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init()  {
+	cobra.OnInitialize(initConfig)
+
+
+}
+
+func initConfig()  {
+	if cfgFile!=""{
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	}else{
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			//er(err)
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cobra")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
